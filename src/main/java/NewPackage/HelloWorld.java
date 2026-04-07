87be6debae206827df7a7af508eea5833d64b25e
KizKnife <134030696+KizKnife@users.noreply.github.com>
2026-04-07 15:44:12 -0500

    Step 10: Print something else in the main method

--- src/main/java/NewPackage/HelloWorld.java
+++ src/main/java/NewPackage/HelloWorld.java
@@ -3,5 +3,6 @@ package NewPackage;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("something else");
     }
 }
